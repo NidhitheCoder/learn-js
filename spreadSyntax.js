9b5f988d9fdec63c1a22546be1d07d202975b324
NidhitheCoder <nidhpv@gmail.com>
2021-01-09 21:43:52 +0530

    Spred syntax

--- spreadSyntax.js
+++ spreadSyntax.js
@@ -1,20 +1,43 @@
-// Objects 
-const pikachu = {name:"Pikachu"};
-const stats = { hp:40,attack:60,defence:40};
-// Bad object code
-pikachu['hp']=stats.hp;
-pikachu['attack'] = stats.attack;
-pikachu['defence'] = stats.defence;
-console.log(pikachu);
+// // Objects 
+// const pikachu = {name:"Pikachu"};
+// const stats = { hp:40,attack:60,defence:40};
+// // Bad object code
+// pikachu['hp']=stats.hp;
+// pikachu['attack'] = stats.attack;
+// pikachu['defence'] = stats.defence;
+// console.log(pikachu);
 
-// OR 
-const lvl0 = Object.assign(pikachu,stats);
-console.log(lvl0);
-const lvl1 = Object.assign(pikachu,{hp:40});
-console.log(lvl1);
+// // OR 
+// const lvl0 = Object.assign(pikachu,stats);
+// console.log(lvl0);
+// const lvl1 = Object.assign(pikachu,{hp:40});
+// console.log(lvl1);
 
-// Good Object code
-const lvl11 = {...pikachu,...stats};
-console.log(lvl11);
-const lvl12 = {...pikachu,hp:35};
-console.log(lvl12);
+// // Good Object code
+// const lvl11 = {...pikachu,...stats};
+// console.log(lvl11);
+// const lvl12 = {...pikachu,hp:35};
+// console.log(lvl12);
+
+
+// Spread syntax in array
+let pokemon = ['Arbok','Raichu','Sandshrew'];
+
+// Bad code
+pokemon.push('Bulbasaur');
+pokemon.push('Metapod');
+console.log(pokemon);
+
+// Good array Code 
+// push
+pokemon = [...pokemon,'Dora',"Buji"];
+console.log(pokemon);
+
+// unshift
+pokemon = ['Dheera','Spiderman',...pokemon];
+console.log(pokemon);
+
+// insert into the middle
+
+pokemon = ['Hulk',...pokemon,'Batman'];
+console.log(pokemon);
