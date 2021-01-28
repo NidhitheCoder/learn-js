bda89d2f373f7cdb5388667cf3eaaed0e1eb5a1f
NidhitheCoder <nidhpv@gmail.com>
2021-01-28 23:17:21 +0530

    Js function created

---
+++ animeJs/main.js
@@ -0,0 +1,28 @@
+const stage = document.querySelector('.container');
+const fragment = document.createDocumentFragment();
+const grid = [20,20];
+const col = grid[0];
+const row = grid[1];
+const field = col * row;
+
+for (let i = 0; i < field; i++) {
+    const div = document.createElement('div');
+    fragment.appendChild(div);
+    div.className = 'tail';
+}
+
+stage.appendChild(fragment);
+
+const stageAnimation = anime.taimeline({
+    targets:'.tail',
+    easing:'easeInBack',
+    delay:anime.stagger(10,{from:'last'}),
+    duration:2000,
+    endDelay:1000,
+    loop:true,
+    autoplay:false,
+})
+
+.add({
+    translateX()
+})
