39db18be10e0adaf1abb21144f27e118fd799538
NidhitheCoder <nidhpv@gmail.com>
2021-06-16 23:54:52 +0530

    lodash basics completed

--- lodash/main.js
+++ lodash/main.js
@@ -16,4 +16,13 @@ const diffArr = _.difference(myList, [ 'Banana', 'Orange' ]);
 
 // intersection pick the items have both arrays - if same item is repeated in same arrays it will not consider as a new item.
 const intArr = _.intersection(myList, [ 'Grapes', 'Apple' ]);
-console.log(intArr);
+
+// Join return a whole string of array values with a separator that we given
+const joinArr = _.join(myList, '-');
+
+// without return array items that exclude the given item- we can use multiple items
+const excludeArr = _.without(myList, 'Apple','Grapes');
+
+// unique return with values of  array that without duplicate item
+const uniqueArr = _.uniq(myList);
+console.log(uniqueArr);
