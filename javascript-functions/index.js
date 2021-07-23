a7bacb4466ea61aa254a3924cdbc183dd1722d09
NidhitheCoder <nidhpv@gmail.com>
2021-07-23 23:58:12 +0530

    Arrow functions pure functions impure functions

--- javascript-functions/index.js
+++ javascript-functions/index.js
@@ -1,26 +1,69 @@
-// Declaration
-const loves = makeBread(7); // It works..
-console.log(loves);
+// // Declaration
+// const loves = makeBread(7); // It works..
+// console.log(loves);
 
-function makeBread(qty) {
-  return 'Bread'.repeat(qty);
+// function makeBread(qty) {
+//   return 'Bread'.repeat(qty);
+// }
+
+// // Function expression
+// const beers = makeBeers(5); // Error: makeBeers is not defined.
+// console.log(beers);
+// const makeBeer = function(qty) {
+//   return 'Beer'.repeat(qty);
+// }
+
+// // IIFE
+// (function(){
+//   console.log("haiii");
+// }());
+
+// // arguments keyword
+// function fun(comedy, song, play) {
+//   console.log(arguments);
+// }
+
+// fun("Ha ha", "Ooo la la", "Football");
+
+// Arrow functions - arrow functions have there is no this objects
+const makeWine = (qty) => "Wine".repeat(qty);
+console.log(makeWine(6));
+
+function dog() {
+  let self = this;
+  this.breed = "Wolf";
+  setTimeout(function () {
+    console.log(this.breed);
+    console.log(self.breed);
+  }, 0);
 }
 
-// Function expression
-const beers = makeBeers(5); // Error: makeBeers is not defined.
-console.log(beers);
-const makeBeer = function(qty) {
-  return 'Beer'.repeat(qty);
+function cat() {
+  this.color = "White";
+  setTimeout(() => {
+    console.log(this.color);
+  }, 0);
 }
 
-// IIFE
-(function(){
-  console.log("haiii");
-}());
+dog();
+cat();
 
-// arguments keyword
-function fun(comedy, song, play) {
-  console.log(arguments);
-}
+// Pure functions
 
-fun("Ha ha", "Ooo la la", "Football");
+let x = 0;
+
+const impure = () => {
+  x++;
+  return x ** 2;
+};
+
+const impure2 = () => {
+  x--;
+  return x.toString();
+};
+
+console.log(impure());
+console.log(impure2());
+
+const pure = (x) => x+3;
+console.log(pure(4));
