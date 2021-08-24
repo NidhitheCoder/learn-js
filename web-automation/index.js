1e5401c1a6d4fbdac6ebe3341f01e3aaae3f2571
NidhitheCoder <nidhpv@gmail.com>
2021-08-24 23:09:25 +0530

    script added

--- web-automation/index.js
+++ web-automation/index.js
@@ -3,8 +3,24 @@ const puppeteer = require('puppeteer');
 (async() => {
   const browser = await puppeteer.launch({ headless: false});
   const page = await browser.newPage();
-  await page.goto('https://cj-dev-portal.ses.vonnue.dev/');
+  await page.goto('https://cj-dev-portal.ses.vonnue.dev/',  {
+    waitUntil: 'networkidle2',
+  });
   await page.screenshot({path: 'screen1.png'});
-    await browser.waitForTarget(() => false);
+    // await browser.waitForTarget(() => false);
+  const result = await page.evaluate(() => {
+    console.log('haiii');
+    const btns = document.querySelectorAll('a button');
+    const buttons = [...btns];
+    // console.log('haiii');
+    let loginBtn = buttons[2];
+    // console.log(loginBtn);
+    // loginBtn.addEventListener('click', () => {
+    //   console.log('login clicked');
+    // })
+    return loginBtn;
+  });
+
+  console.log(result);
   await browser.close();
 })();
