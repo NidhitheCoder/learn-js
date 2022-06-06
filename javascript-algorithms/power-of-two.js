2cedddc52c981468210e84297bd5ad27ec10ea44
NidhitheCoder <nidhpv@gmail.com>
2022-06-06 23:54:55 +0530

    feat: Power of two optimal solutions

--- javascript-algorithms/power-of-two.js
+++ javascript-algorithms/power-of-two.js
@@ -6,22 +6,39 @@
 // if reminder is not 0 in any step , 'n' is not a power of two 
 // if the reminder is 0 and 'n' comes down to 1 eventually, n is a power of two.
 
-const isPowerOftwo = (n) => {
+// const isPowerOftwo = (n) => {
 
-  if (n < 1) {
+//   if (n < 1) {
+//     return false;
+//   }
+
+//   while (n > 1) {
+//     if (n % 2 !== 0) {
+//       return false;
+//     }
+//     n = n / 2;
+//   }
+
+//   return true;
+// };
+
+// console.log(isPowerOftwo(1));
+// console.log(isPowerOftwo(2));
+// console.log(isPowerOftwo(5));
+
+// Big-O = O(logn)
+
+
+const isPowerOfTwoBitwise = (n) => {
+  if(n < 1) {
     return false;
   }
-
-  while (n > 1) {
-    if (n % 2 !== 0) {
-      return false;
-    }
-    n = n / 2;
-  }
-
-  return true;
+  return (n & n-1) === 0
 };
 
-console.log(isPowerOftwo(1));
-console.log(isPowerOftwo(2));
-console.log(isPowerOftwo(5));
+
+console.log(isPowerOfTwoBitwise(1));
+console.log(isPowerOfTwoBitwise(2));
+console.log(isPowerOfTwoBitwise(5));
+
+// Big O = O(1)
