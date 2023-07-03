b18f936bbac525d0ee16cec8cf677e338e5cb5e5
Nidheesh PV <nidheesh@vonnue.com>
2023-07-03 23:53:56 +0530

    feat; toSort method and with method

--- arrays/new-array-methods.js
+++ arrays/new-array-methods.js
@@ -1,6 +1,16 @@
 const people = ["Sam", "Sara", "Ellisa"];
 
-const peopleCopy = [...people];
+// // Simplest way
+// const peopleCopy = [...people];
 
-peopleCopy[2] = "new";
-console.log({ original: people, copy: peopleCopy });
+// peopleCopy[2] = "new";
+// console.log({ original: people, copy: peopleCopy });
+
+// const newPeople = people.with(2, "New"); // Working only the version after v20.0.0
+// console.log(newPeople);
+
+const sortedPeople = [...people].sort();
+const toSortedPeople = people.toSorted();  // Working only the version after v20.0.0
+console.log(people);
+console.log(sortedPeople);
+console.log(toSortedPeople);
