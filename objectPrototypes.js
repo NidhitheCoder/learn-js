f4910a2f7818086c5ceb8123f807434d34c2f61d
Nidheesh PV <nidheesh@vonnue.com>
2023-10-31 23:51:53 +0530

    feat: learn shadowing propery

--- objectPrototypes.js
+++ objectPrototypes.js
@@ -8,5 +8,28 @@ const myObj = {
     }
 }
 
-myObj.center.greet();
-console.log(myObj.toString())
+// myObj.center.greet();
+// console.log(myObj.toString())
+
+const myDate = new Date();
+let object = myDate;
+
+console.log(Object.getPrototypeOf(object))
+
+do {
+    object = Object.getPrototypeOf(object);
+    console.log("____")
+    console.log(object);
+    console.log("_____;")
+} while (object)
+
+// Shadowing property
+console.log(myDate.getFullYear())
+
+console.log(myDate.getYear())
+
+myDate.getYear = function () {
+    console.log("something else")
+}
+
+myDate.getYear();
