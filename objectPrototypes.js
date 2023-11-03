dc87aad739c9a76ec16d0d7c16a825a57fb8222f
Nidheesh PV <nidheesh@vonnue.com>
2023-11-03 22:41:16 +0530

    feat: implement prototypes using contructor

--- objectPrototypes.js
+++ objectPrototypes.js
@@ -14,22 +14,57 @@ const myObj = {
 const myDate = new Date();
 let object = myDate;
 
-console.log(Object.getPrototypeOf(object))
+// console.log(Object.getPrototypeOf(object))
 
 do {
     object = Object.getPrototypeOf(object);
-    console.log("____")
-    console.log(object);
-    console.log("_____;")
+    // console.log("____")
+    // console.log(object);
+    // console.log("_____;")
 } while (object)
 
 // Shadowing property
-console.log(myDate.getFullYear())
+// console.log(myDate.getFullYear())
 
-console.log(myDate.getYear())
+// console.log(myDate.getYear())
 
 myDate.getYear = function () {
     console.log("something else")
 }
 
-myDate.getYear();
+// myDate.getYear();
+
+// const personPrototype = {
+//     greet() {
+//         console.log("Welcome !")
+//     }
+// }
+// console.log(personPrototype)
+// personPrototype.greet();
+
+// const carl = Object.create(personPrototype);
+
+// console.log(carl)
+
+// carl.greet();
+
+
+// Using constructor
+const personPrototype = {
+    greet() {
+        console.log("Welcome !")
+    }
+}
+
+
+function Person(name) {
+    this.name = name
+}
+
+Object.assign(Person.prototype, personPrototype)
+
+const ruben = new Person("Ruben");
+
+console.log(ruben)
+
+ruben.greet();
